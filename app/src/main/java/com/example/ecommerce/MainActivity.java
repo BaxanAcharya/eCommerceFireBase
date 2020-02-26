9514030b1c274f4e68a72d54cd553d54ef639a83
BaxanAcharya <baxanacharya@gmail.com>
2020-02-26 23:01:39 +0545

    'login design complete'

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnlogin=findViewById(R.id.main_login_btn);
         join_now=findViewById(R.id.main_jowin_now);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
