d04b2ce46b3ce5ed6683c61b993f2b1a6c0ab55f
BaxanAcharya <baxanacharya@gmail.com>
2020-02-26 22:06:15 +0545

    main activity design complete

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnlogin,join_now;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnlogin=findViewById(R.id.main_login_btn);
+        join_now=findViewById(R.id.main_jowin_now);
     }
 }
